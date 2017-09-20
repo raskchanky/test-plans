a46a38d582442a180b1d49fcaf3085201e94f835
Salim Alam <salam@chef.io>
2017-09-19 17:04:25 -0700

    Add core dep to testpkg3
    Signed-off-by: Salim Alam <salam@chef.io>

--- testpkg3/plan.sh
+++ testpkg3/plan.sh
@@ -5,7 +5,7 @@ pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=()
 pkg_upstream_url=https://github.com/habitat-sh/habitat-example-plans
 pkg_source=nosuchfile.tar.gz
-pkg_deps=()
+pkg_deps=(core/testapp)
 pkg_expose=()
 
 do_download() {
