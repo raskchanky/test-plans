59ecb7893de580520ae6e41f2d07c6de87db5cba
Salim Alam <salam@chef.io>
2018-07-09 16:16:25 -0700

    New testapp

---
+++ testapp/plan.sh
@@ -0,0 +1,31 @@
+pkg_origin=chefsalim
+pkg_name=testapp
+pkg_version=0.1.0
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_license=()
+pkg_upstream_url=https://github.com/habitat-sh/habitat-example-plans
+pkg_source=nosuchfile.tar.gz
+pkg_deps=()
+pkg_expose=()
+pkg_bin_dirs=(bin)
+pkg_svc_run="sleep 10000"
+
+do_download() {
+  return 0
+}
+
+do_verify() {
+  return 0
+}
+
+do_unpack() {
+  return 0
+}
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  return 0
+}
