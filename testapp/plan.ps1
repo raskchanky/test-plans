4e4d0dcd8b8d21cdc86f5f89bdc71de5423289db
Salim Alam <salam@chef.io>
2019-01-16 18:48:29 -0800

    Add plan.ps1 to testapp
    Signed-off-by: Salim Alam <salam@chef.io>

---
+++ testapp/plan.ps1
@@ -0,0 +1,23 @@
+$pkg_origin="chefsalim"
+$pkg_name="testapp"
+$pkg_version="0.1.0"
+$pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+$pkg_upstream_url="https://github.com/habitat-sh/habitat-example-plans"
+$pkg_source="nosuchfile.tar.gz"
+$pkg_bin_dirs=@("bin")
+$pkg_svc_run="sleep 10000"
+
+function Invoke-Download {
+}
+
+function Invoke-Verify {
+}
+
+function Invoke-Unpack {
+}
+
+function Invoke-Build {
+}
+
+function Invoke-Install {
+}
